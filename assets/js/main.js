669eb7dabad906f453817e3a011d798e423b769c
Daniel Simon Gualde <daniel.simon.gualde@gmail.com>
2023-01-30 21:44:48 +0100

    Creamos el index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-const navMenu=document.getElementById("nav-menu"),
+const navMenu = document.getElementById("nav-menu"),
     navToggle = document.getElementById('nav-toggle'),
-    navClose= document.getElementById('nav-close')
+    navClose = document.getElementById('nav-close')
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
+if (navToggle) {
     console.log("saf")
-    navToggle.addEventListener('click',()=>{
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add("show-menu")
 
     })
@@ -14,9 +14,9 @@ if(navToggle){
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click' , () =>{
-navMenu.classList.remove('show-menu')
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
 
     })
 }
@@ -24,16 +24,30 @@ navMenu.classList.remove('show-menu')
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
-const navMenu = document.getElementById('nav-menu')
-navMenu.classList.remove('show-menu');
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu');
 
 }
-navLink.forEach(n => n.addEventListener('click',linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
 
+function toggleSkills() {
+    let ItemClass = this.parentNode.className
+    for (i = 0; i < skillsContent.length; i++) {
 
+        skillsContent[i].className = "skills__content skills__close"
+    }
+    if(ItemClass === 'skills__content skills__close'){
+        this.parentNode.className= 'skills__content skills__open'
+    }
+}
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
 /*==================== QUALIFICATION TABS ====================*/
 
 
@@ -49,10 +63,10 @@ navLink.forEach(n => n.addEventListener('click',linkAction))
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
 
 /*==================== DARK LIGHT THEME ====================*/ 
